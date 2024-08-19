4dfe804d8f973f7d580f535fc2363966bf02461a
Atul <99653239+atulranjan9026@users.noreply.github.com>
2024-08-19 16:38:23 +0530

    api

--- classroom-backend/app.js
+++ classroom-backend/app.js
@@ -7,7 +7,7 @@ app.use(express.json());
 
 
 app.use(cors({
-  origin: 'http://127.0.0.1:5173',
+  origin: 'https://classroom-management-qmbt.vercel.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
